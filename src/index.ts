89acb7bbe5f87081f84a6cab3c358aaea03de5f9
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-16 23:31:14 -0400

    primer observable

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Observable, Subscriber } from "rxjs";
+
+//const obs$ = Observable.create()
+const obs$ = new Observable<String>( subs => {
+    subs.next('Hola')
+    subs.next('Mundo')
+
+    subs.next('Hola')
+    subs.next('Mundo')
+
+    subs.complete()
+    
+    subs.next('Hola')
+    subs.next('Mundo')
+});
+
+obs$.subscribe(console.log)
