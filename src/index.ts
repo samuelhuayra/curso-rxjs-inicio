fc0ee9f187ad5be6d86dd2d17bad90e148b05cc2
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-16 23:49:22 -0400

    Observer and Subscriptions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import { Observable, Subscriber } from "rxjs";
+import { Observable, Subscriber, Observer } from "rxjs";
 
+//Method 1
+const observer:Observer<any>={
+    next: value=> console.log('Siguiente [next]: ',value),
+    error: error=> console.warn('Error [obs]: ',error),
+    complete: ()=>console.log('Complete [obs]')
+}
+
+//Method 2
 //const obs$ = Observable.create()
 const obs$ = new Observable<String>( subs => {
     subs.next('Hola')
@@ -8,10 +16,23 @@ const obs$ = new Observable<String>( subs => {
     subs.next('Hola')
     subs.next('Mundo')
 
+    //forzar error
+    /* const a = undefined
+    a.nombre = 'Samuel' */
+
+
     subs.complete()
     
     subs.next('Hola')
     subs.next('Mundo')
 });
 
-obs$.subscribe(console.log)
+//Method 1
+obs$.subscribe(observer);
+
+//Method 2
+/* obs$.subscribe(
+    valor=> console.log('next: ',valor),
+    error=> console.warn('error: ',error),
+    ()=>console.log('Complete')
+) */
