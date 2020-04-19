efb1ea387a86b41a322529b10f9b910260fe673d
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-19 01:46:16 -0400

    mergeAll example RxJS

--- src/transformacion/01-mergeAll.ts
+++ src/transformacion/01-mergeAll.ts
@@ -1,4 +1,4 @@
-import { fromEvent, Observable } from "rxjs"
+import { fromEvent, Observable, interval } from "rxjs"
 import { debounceTime, map, pluck, mergeAll } from "rxjs/operators"
 import { ajax } from "rxjs/ajax"
 import { GithubUser } from "../interfaces/github-user.interface"
@@ -37,12 +37,23 @@ const input$ = fromEvent<KeyboardEvent>(textInput,'keyup')
 input$.pipe(
     debounceTime<KeyboardEvent>(500),
     pluck<KeyboardEvent,string>('target','value'),
+    //Ej: map devuelve observables 
     map<string,Observable<GithubUsers>>(texto=>
         ajax.getJSON(`https://api.github.com/search/users?q=${texto}`)
     ),
+    //Entonces mergeAll va a suntar todos los observables
     mergeAll<GithubUsers>(), //???
     pluck<GithubUsers,GithubUser[]>('items')
 ).subscribe(mostrarUsuarios)
 
 //Crear interfaces de un json
 //https://app.quicktype.io/
+
+//ejemplo de RxJS
+const clicks = fromEvent(document, 'click');
+//map devuelve observables
+const higherOrder = clicks.pipe(map((ev) => interval(1000)));
+//Merge all va a unir esos observables en 1
+const firstOrder = higherOrder.pipe(mergeAll());
+
+firstOrder.subscribe(x => console.log(x));
