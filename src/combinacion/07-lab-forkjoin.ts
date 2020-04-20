01827f215b9276d52160f63004282b1e167fbb0e
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-20 00:07:03 -0400

    lab-forkjoin

---
+++ src/combinacion/07-lab-forkjoin.ts
@@ -0,0 +1,22 @@
+import { forkJoin, of } from "rxjs"
+import { ajax } from "rxjs/ajax"
+import { catchError } from "rxjs/operators"
+
+const GITHUB_API_URL = 'https://api.github.com/users'
+const GITHUB_USER = 'klerith'
+
+forkJoin({
+    user: ajax.getJSON(
+        `${GITHUB_API_URL}/${GITHUB_USER}`
+    ),
+    repos: ajax.getJSON(
+        `${GITHUB_API_URL}/${GITHUB_USER}/reposxxx`
+    ).pipe(
+        catchError(err=>of([]))
+    ),
+    gists: ajax.getJSON(
+        `${GITHUB_API_URL}/${GITHUB_USER}/gists`
+    ),
+}).pipe(
+    catchError(err=>of(err.message))
+).subscribe(console.log)
