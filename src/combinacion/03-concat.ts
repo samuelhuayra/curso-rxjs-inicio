9251a9b1e344aedda751621fe4d1397e80a70e62
Samuel Huayra <samuelhuayra@gmail.com>
2020-04-19 23:36:58 -0400

    concat

---
+++ src/combinacion/03-concat.ts
@@ -0,0 +1,12 @@
+import { interval, concat, of } from "rxjs";
+import { take } from "rxjs/operators";
+
+
+const interval$ = interval(1000)
+
+concat(
+    interval$.pipe(take(3)),
+    interval$.pipe(take(2)),
+    of(5,6,7)
+).subscribe(console.log)
+
